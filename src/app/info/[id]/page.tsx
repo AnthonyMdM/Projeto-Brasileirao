de85bddf449da9e17a2c8cb54d0d17b39c6c3c20
Anthony Mariano <33204365+AnthonyMdM@users.noreply.github.com>
2025-07-08 21:03:46 -0300

    Correção ID:Params

--- src/app/info/[id]/page.tsx
+++ src/app/info/[id]/page.tsx
@@ -5,23 +5,24 @@ import TelaInfo from "@/views/TelaInfoTime";
 export async function generateMetadata({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }): Promise<Metadata> {
-  const id = params?.id;
+  const { id } = await params;
 
   const entrada = Object.entries(infoTime).find(([, info]) => info.url === id);
-
   const nomeOriginal = entrada?.[0] ?? "Time não encontrado";
 
   return {
     title: `Informações do ${nomeOriginal}`,
     description: `Detalhes do time ${nomeOriginal} na temporada atual.`,
     icons: {
-      icon: `${infoTime[nomeOriginal].caminho}`,
+      icon: `${infoTime[nomeOriginal]?.caminho ?? "/default-icon.png"}`,
     },
   };
 }
 
-export default async function Page({ params }: { params: { id: string } }) {
-  return <TelaInfo id={params.id} />;
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+
+  return <TelaInfo id={id} />;
 }
